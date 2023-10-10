e4641629c47b09f5358b449be7414a56cab4c52a
AVWORLD\jose9489 <JBanuelos@esri.com>
2023-10-10 15:31:00 -0700

    Adding new rule based snapping tester app

---
+++ 4x/rule-based-snapping-tester/README.md
@@ -0,0 +1,21 @@
+# Rule Based Snapping Tester
+
+This project uses the ArcGIS JavaScript API 4.x to test Utility Network snapping rules. The application brings in the Editor and the RulesTable to constrain what features can be snapped to in editing workflows.
+
+This application displays the rules table with all the rules in the utility network rules feature service in its own UI container. There is also a container that lists all the versions available to the logged in user to switch to, since we do not want to edit the default SDE version!
+
+<img src="snap-app.png" width="800"/>
+
+## Live App
+
+[Rule Based Snapping Tester](https://banuelosj.github.io/jsapi-samples/4x/rule-based-snapping-tester)
+
+## Built With
+
+* [ArcGIS JavaScript API](https://developers.arcgis.com/javascript/) - Using the 4.28 JavaScript API
+
+## Relevant API
+* WebMap
+* Editor
+* VersionManagementService
+* FeatureService
---
+++ 4x/rule-based-snapping-tester/js/main.js
@@ -0,0 +1,336 @@
+require([
+  "esri/WebMap", 
+  "esri/views/MapView", 
+  "esri/widgets/Editor",
+  "esri/widgets/LayerList",
+  "esri/widgets/Legend",
+  "esri/core/reactiveUtils",
+  "esri/versionManagement/VersionManagementService",
+  "esri/rest/featureService/FeatureService"
+], function(
+  WebMap,
+  MapView,
+  Editor,
+  LayerList,
+  Legend,
+  reactiveUtils,
+  VersionManagementService,
+  FeatureService
+) {
+
+  let utilityNetwork, rulesTable;
+  let activeWidget;
+  let portalOrg, webMapId;
+  let editor;
+  let view;
+  let fs, vms;
+
+  document.querySelector(`[data-action-id=layers]`).active = true;
+  document.querySelector(`[data-panel-id=layers]`).hidden = false;
+
+  const modalSaveBtn = document.getElementById("modal-save-btn");
+  modalSaveBtn.onclick = () => { submitModal() };
+
+  function submitModal() {
+    portalOrg = document.getElementById("modal-org-input").value;
+    webMapId = document.getElementById("modal-webmap-input").value;
+
+    let orgId = !portalOrg ? "https://arcgis.com" : portalOrg;
+    const webmap = new WebMap({
+      portalItem: {
+        portal: {
+          url: orgId
+        },
+        id: webMapId
+      }
+    });
+    loadInitialMap(webmap);
+    document.getElementById("webmap-modal").open = false;
+  }
+
+  function loadInitialMap(webmap) {
+    view = new MapView({
+      container: "viewDiv",
+      map: webmap
+    });
+
+    editor = new Editor({
+      view: view
+    });
+    view.ui.add(editor, "top-right");
+
+    const layerList = new LayerList({ view, container: "layers-container" });
+    const legend = new Legend({ view, container: "legend-container" });
+
+    view.when(async () => {
+      await view.map.loadAll();
+
+      editor.snappingOptions.enabled = true;
+      editor.snappingOptions.featureEnabled = true;
+      enableSnappingOnLayers(view.map.editableLayers.items);
+ 
+      loadFSAndVMS(view.map.editableLayers.items);
+
+      // Check if webmap contains utility networks.
+      if (webmap?.utilityNetworks?.length > 0) {
+        //setVersion(view.map.layers.items, MY_VERSION);
+
+        // Assigns the utility network at index 0 to utilityNetwork. 
+        utilityNetwork = webmap.utilityNetworks.getItemAt(0);
+
+        // Triggers the loading of the UtilityNetwork instance.
+        await utilityNetwork.load();
+
+        // load the rules table
+        rulesTable = await utilityNetwork.getRulesTable();
+        await rulesTable.load();
+        displayRulesInPanel(rulesTable);
+      }
+
+      setPortalOrgWebMapId(view.map);
+    });
+  }
+  
+
+  // UI
+  // Add the widget to the view
+  
+  document.querySelector("calcite-action-bar").addEventListener("click", handleActionBarClick);
+  document.querySelector("calcite-shell").hidden = false;
+  const list = document.getElementById("listEl");
+  const versionList = document.getElementById("versionListEl");
+  versionList.addEventListener("calciteListChange", handleVersionChange)
+  let originalList = null;
+  let originalVersionList = null;
+  const filterInput = document.getElementById("filterInput");
+  filterInput.addEventListener("calciteInputTextInput", handleInputFilter);
+  const versionsFilterInput = document.getElementById("versionsFilterInput");
+  versionsFilterInput.addEventListener("calciteInputTextInput", handleInputVersionsFilter);
+  const orgNameInput = document.getElementById("orgNameInput");
+  const webmapIdInput = document.getElementById("webmapIdInput");
+  const updateBtn = document.getElementById("updateBtn");
+  updateBtn.onclick = () => { updateWebMap() };
+  const versionActionBtn = document.getElementById("version-action-btn")
+
+  // input event listening
+  orgNameInput.addEventListener('calciteInputInput', (evt) => {
+    updateBtn.disabled = false;
+  });
+
+  webmapIdInput.addEventListener('calciteInputInput', (evt) => {
+    updateBtn.disabled = false;
+  });
+
+  function setVersion(layers, version) {
+    layers.forEach((layer) => {
+      if(layer.type === "group") {
+        layer.layers.items.forEach((sublayer) => {
+          sublayer.gdbVersion = version;
+        });
+      }
+      if(layer.type === "feature") {
+        layer.gdbVersion = version;
+      }
+      if (layer.type === "subtype-group") {
+        layer.gdbVersion = version;
+      }
+    });
+  }
+
+  function handleActionBarClick({ target }) {
+    if (target.tagName !== "CALCITE-ACTION") {
+      return;
+    }
+
+    document.querySelector(`[data-action-id=layers]`).active = false;
+    document.querySelector(`[data-panel-id=layers]`).hidden = true;
+
+    const nextWidget = target.dataset.actionId;
+
+    if (activeWidget) {
+      document.querySelector(`[data-action-id=${activeWidget}]`).active = false;
+      document.querySelector(`[data-panel-id=${activeWidget}]`).hidden = true;
+    }
+    
+    if (nextWidget !== activeWidget) {
+      document.querySelector(`[data-action-id=${nextWidget}]`).active = true;
+      document.querySelector(`[data-panel-id=${nextWidget}]`).hidden = false;
+      
+      activeWidget = nextWidget;
+    } else {
+      activeWidget = null;
+    }
+  }
+
+  function displayRulesInPanel({ rules }) {
+    list.innerHTML = "";
+    rules.forEach((item) => {
+      const { ruleType, fromAssetGroup, fromAssetType, toAssetGroup, toAssetType } = item;
+      const listItem = document.createElement("calcite-list-item");
+      listItem.label = getRuleNameFromCode(ruleType);
+      const descriptionString = `${fromAssetGroup.assetGroupName} | ${fromAssetType.assetTypeName} (${fromAssetType.connectivityPolicy}) | ${toAssetGroup.assetGroupName} | ${toAssetType.assetTypeName}`;
+      listItem.description = descriptionString;
+      list.append(listItem);
+    });
+    originalList = [...list.children];
+  }
+
+  function displayVersionsInPanel(versions) {
+    versionActionBtn.indicator = true;
+    versionList.innerHTML = "";
+    versions.forEach((item) => {
+      const { access, description, versionIdentifier } = item;
+      const listItem = document.createElement("calcite-list-item");
+      listItem.label = `(${access}) ${versionIdentifier.name}`;
+      listItem.description = versionIdentifier.guid;
+      listItem.value = versionIdentifier.name;
+      // select default version
+      if (vms.defaultVersionIdentifier.name === versionIdentifier.name) {
+        listItem.selected = true;
+      }
+      versionList.append(listItem);
+    }); 
+    originalVersionList = [...versionList.children];
+  }
+
+  function getRuleNameFromCode(code) {
+    switch (code) {
+      case 1:
+        return "Junction-Junction Connectivity";
+        break;
+      case 2:
+        return "Containment";
+        break;
+      case 3:
+        return "Structural Attachment";
+        break;
+      case 4:
+        return "Junction-Edge Connectivity";
+        break;
+      case 5:
+        return "Edge-Junction-Edge Connectivity";
+        break;
+      default:
+        return "Unknown";
+    }
+  }
+
+  function handleInputFilter(evt) {
+    let { value } = evt.target;
+    value = value.toLowerCase();
+    
+    let filteredResults = [...originalList];
+    filteredResults = filteredResults.filter((item) => item.description.toLowerCase().includes(value));
+    list.replaceChildren(...filteredResults) ;
+  }
+
+  function handleInputVersionsFilter(evt) {
+    let { value } = evt.target;
+    value = value.toLowerCase();
+    
+    let filteredResults = [...originalVersionList];
+    filteredResults = filteredResults.filter((item) => item.label.toLowerCase().includes(value));
+    versionList.replaceChildren(...filteredResults) ;
+  }
+
+  function setPortalOrgWebMapId(map) {
+    const { portalItem } = map;
+    orgNameInput.value = portalItem.portal.url;
+    webmapIdInput.value = portalItem.id;
+  }
+
+  async function updateWebMap() {
+    portalOrg = orgNameInput.value;
+    webMapId = webmapIdInput.value;
+
+    view.ui.remove(editor);
+    editor.destroy();
+    view.map.destroy();
+
+    let orgId = !portalOrg ? "https://arcgis.com" : portalOrg;
+
+    const wm = new WebMap({
+      portalItem: {
+        id: webMapId,
+        portal: {
+          url: orgId
+        }
+      }
+    });
+
+    await wm.load();
+    view.map = wm;
+
+    reactiveUtils.watch(
+      () => !view.updating,
+      () => {
+        view.goTo(view.map.initialViewProperties.viewpoint.targetGeometry)
+        addEditor(view.map.editableLayers.items);
+        loadFSAndVMS(view.map.editableLayers.items);
+      }, { once: true }
+    );
+
+    updateBtn.disabled = true;
+
+    // Check if webmap contains utility networks.
+    if (wm.utilityNetworks?.length > 0) {
+      //setVersion(view.map.layers.items, MY_VERSION);
+
+      // Assigns the utility network at index 0 to utilityNetwork. 
+      utilityNetwork = wm.utilityNetworks.getItemAt(0);
+
+      // Triggers the loading of the UtilityNetwork instance.
+      await utilityNetwork.load();
+
+      // load the rules table
+      rulesTable = await utilityNetwork.getRulesTable();
+      await rulesTable.load();
+      displayRulesInPanel(rulesTable);
+    }
+  }
+
+  function addEditor(editableLayers) {
+    editor = new Editor({
+      view: view,
+    });
+    view.ui.add(editor, "top-right");
+
+    editor.snappingOptions.enabled = true;
+    editor.snappingOptions.featureEnabled = true;
+    enableSnappingOnLayers(editableLayers);
+  }
+
+  async function loadFSAndVMS(layers) {
+    // grabbing the first layer
+    // not ideal, might need to update this later
+    const fsUrl = layers[0].url;
+    fs = new FeatureService({ url: fsUrl });
+    await fs.load();
+    if(fs.versionManagementServiceUrl) {
+      vms = new VersionManagementService({ url: fs.versionManagementServiceUrl });
+      await vms.load();
+      let versionInfos = await vms.getVersionInfos();
+      displayVersionsInPanel(versionInfos)
+    }
+  }
+
+  function handleVersionChange(evt) {
+    const { selectedItems } = evt.target;
+    setVersion(selectedItems[0].value);
+  }
+
+  function setVersion(gdbName) {
+    view.map.editableLayers.items.forEach((layer) => {
+      if (layer.type === "feature" || layer.type === "subtype-group") {
+        layer.gdbVersion = gdbName;
+      }
+    });
+  }
+
+  function enableSnappingOnLayers(layers) {
+    layers.forEach((layer) => {
+      editor.snappingOptions.featureSources.push({ layer, enabled: true });
+    });
+  }
+  
+});
